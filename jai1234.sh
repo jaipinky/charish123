a4e49809b51008e8130f985034565fd782ad0340
jaipinky <jai.linux1985@outlook.com>
2018-08-28 19:34:07 +0530

    first message

---
+++ jai1234.sh
@@ -0,0 +1,16 @@
+#!bin\bash
+echo "Please Display System Health Report"
+echo "Display Today date"
+date
+echo "please display user id"
+id
+echo "Please display file system infomaration"
+df -hT
+echo "please display memory info"
+free -m
+echo "please display virtual memory"
+vmstat
+echo "please display who are logged in the system"
+w
+
+
